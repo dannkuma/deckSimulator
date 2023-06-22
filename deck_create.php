a8edbb4f917d33ea06678ce353733407e4479baf
dannkuma <dannk0308@gmail.com>
2023-06-23 08:42:08 +0900

    Sample Upload Test

---
+++ deck_create.php
@@ -0,0 +1,15 @@
+<?php session_start(); ?>
+<?php
+if(!isset($_SESSION['deck'])) {
+    $_SESSION['deck']=[];
+}
+if(in_array($_REQUEST['generalId'], $_SESSION['deck'])) {//デッキ配列にリクエストパラメータが存在するか確認
+    $array_search = array_search($_REQUEST['generalId'], $_SESSION['deck']);//リクエストパラメータのキーを返す
+    unset($_SESSION['deck'][$array_search]);//キーを指定してdeck配列から値を削除
+} else {
+    array_push($_SESSION['deck'], $_REQUEST['generalId']);//配列に要素を追加する
+}
+
+header("Location:" . $_SESSION['url']);
+exit();
+?>
